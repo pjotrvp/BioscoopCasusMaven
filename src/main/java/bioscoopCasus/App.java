db5393cc5d2f824a4ae8fc049644dc36f4f38f5b
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-02-01 15:04:03 +0100

    Test data in main added

--- src/main/java/bioscoopCasus/App.java
+++ src/main/java/bioscoopCasus/App.java
@@ -1,5 +1,8 @@
 package bioscoopCasus;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        movie theMatrix = new movie("The Matrix");
+        movieScreening theMatrixScreening = new movieScreening(theMatrix, LocalDateTime.parse("2023-10-16T18:30"), 7);
+        movieTicket matrixTicket1 = new movieTicket(theMatrixScreening, 1, 1, false);
+        movieTicket matrixTicket2 = new movieTicket(theMatrixScreening, 1, 2, true);
+        order matrixOrder = new order(1, false);
+        matrixOrder.addSeatReservation(matrixTicket1);
+        matrixOrder.addSeatReservation(matrixTicket2);
+        matrixOrder.calculatePrice();
+        System.out.println("main invoked");
+        try {
+            matrixOrder.export(ticketExportFormat.PLAINTEXT);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
